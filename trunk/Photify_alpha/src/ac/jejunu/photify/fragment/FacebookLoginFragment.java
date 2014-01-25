6f33d5406bb4783e18dc8a15aac31c3958ad510e
pop9310@gmail.com <pop9310@gmail.com@03aedb2d-931b-6bb0-fd80-cf2cc3f0306d>
2014-01-25 12:03:07 +0000

    페이스북 유저 엑세스 토큰 받아오기 구현 완료 - 서버로 전송해줘야 하는데..... 서버로 전송하고 서버에선 이 엑세스 토큰으로 글 올리기를 진행해야함.

--- trunk/Photify_alpha/src/ac/jejunu/photify/fragment/FacebookLoginFragment.java
+++ trunk/Photify_alpha/src/ac/jejunu/photify/fragment/FacebookLoginFragment.java
@@ -3,10 +3,13 @@ package ac.jejunu.photify.fragment;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ac.jejunu.photify.R;
-import ac.jejunu.photify.entity.FacebookArticle;
 import ac.jejunu.photify.rest.DefaultRestClient;
 import ac.jejunu.photify.rest.ReadFacebookArticleClient;
 import ac.jejunu.photify.rest.ServerIpAddress;
@@ -15,16 +18,26 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.FacebookException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.FacebookRequestError;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.WebDialog;
 import com.google.gson.Gson;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.Background;
@@ -35,60 +48,59 @@ import com.googlecode.androidannotations.annotations.rest.RestService;
 @EFragment(R.layout.fragment_facebook_login)
 public class FacebookLoginFragment extends Fragment {
 	private static final String TAG = "FacebookLoginFragment";
-
+	
 	@ViewById(R.id.shareButton)
 	Button shareButton;
-
+	
 	@ViewById(R.id.sendRequestButton)
 	Button sendRequestButton;
-
+	
 	@ViewById(R.id.authButton)
 	LoginButton authButton;
-
+	
 	@RestService
 	ReadFacebookArticleClient readArticleClient;
-
+	
 	Gson gson;
-
+	
 	private static final List<String> PERMISSIONS = Arrays.asList("publish_actions", "publish_stream", "read_stream");
-
+	
 	private static final String PENDING_PUBLISH_KEY = "pendingPublishReauthorization";
-
+	
 	private boolean pendingPublishReauthorization = false;
-
+	
 	private UiLifecycleHelper uiHelper;
-
+	
 	private Session.StatusCallback callback = new Session.StatusCallback() {
 		@Override
 		public void call(final Session session, final SessionState state, final Exception exception) {
 			onSessionStateChange(session, state, exception);
 		}
 	};
-
+	
 	@AfterViews
 	void afterViews() {
-		// sendRequestButton.setOnClickListener(new View.OnClickListener() {
-		// @Override
-		// public void onClick(View v) {
-		// sendRequestDialog();
-		// }
-		// });
-		//
-		// authButton.setFragment(this);
-		// shareButton.setOnClickListener(new View.OnClickListener() {
-		// @Override
-		// public void onClick(View v) {
-		// publishStory();
-		// }
-		// });
+		sendRequestButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				sendRequestDialog();
+			}
+		});
 
-		login();
-		checkLogin();
+		authButton.setFragment(this);
+		shareButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				publishStory();
+			}
+		});
+
+		printHashKey();
 	}
-
+	
 	@Background
 	void login() {
-
+		
 		DefaultRestClient rc = new DefaultRestClient();
 		try {
 			String result = rc.post(ServerIpAddress.IP + "/login.photo", "fbid", "123123");
@@ -97,7 +109,7 @@ public class FacebookLoginFragment extends Fragment {
 			e.printStackTrace();
 		}
 	}
-
+	
 	@Background
 	void checkLogin() {
 		DefaultRestClient rc = new DefaultRestClient();
@@ -108,7 +120,7 @@ public class FacebookLoginFragment extends Fragment {
 			e.printStackTrace();
 		}
 	}
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -118,11 +130,11 @@ public class FacebookLoginFragment extends Fragment {
 			pendingPublishReauthorization = savedInstanceState.getBoolean(PENDING_PUBLISH_KEY, false);
 		}
 	}
-
+	
 	@Override
 	public void onResume() {
 		super.onResume();
-
+		
 		// For scenarios where the fragment_facebook_login activity is launched
 		// and user
 		// session is not null, the session state change notification
@@ -130,36 +142,37 @@ public class FacebookLoginFragment extends Fragment {
 		Session session = Session.getActiveSession();
 		if (session != null && (session.isOpened() || session.isClosed())) {
 			onSessionStateChange(session, session.getState(), null);
+			Log.e("users access token ", session.getAccessToken());
 		}
-
+		
 		uiHelper.onResume();
 	}
-
+	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		uiHelper.onActivityResult(requestCode, resultCode, data);
 	}
-
+	
 	@Override
 	public void onPause() {
 		super.onPause();
 		uiHelper.onPause();
 	}
-
+	
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		uiHelper.onDestroy();
 	}
-
+	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putBoolean(PENDING_PUBLISH_KEY, pendingPublishReauthorization);
 		uiHelper.onSaveInstanceState(outState);
 	}
-
+	
 	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
 		Log.e("onSessionStateChange", "state : isOpen?:" + state.isOpened());
 		if (state.isOpened()) {
@@ -174,118 +187,97 @@ public class FacebookLoginFragment extends Fragment {
 			sendRequestButton.setVisibility(View.INVISIBLE);
 		}
 	}
-
-	// private void publishStory() {
-	// Session session = Session.getActiveSession();
-	// if (session != null) {
-	//
-	// // Check for publish permissions
-	// List<String> permissions = session.getPermissions();
-	// if (!isSubsetOf(PERMISSIONS, permissions)) {
-	// pendingPublishReauthorization = true;
-	// Session.NewPermissionsRequest newPermissionsRequest = new
-	// Session.NewPermissionsRequest(this, PERMISSIONS);
-	// session.requestNewPublishPermissions(newPermissionsRequest);
-	// return;
-	// }
-	//
-	// Bundle postParams = new Bundle();
-	// postParams.putString("name", "Facebook SDK for Android");
-	// postParams.putString("caption",
-	// "Build great social apps and get more installs.");
-	// postParams.putString("description",
-	// "The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
-	// postParams.putString("link", "https://developers.facebook.com/android");
-	//
-	// String appStorage = Environment.getExternalStorageDirectory().getPath();
-	// String path = appStorage + "/PolarClock_background/background.png";
-	// // addImage(postParams, path);
-	//
-	// postParams.putString("picture",
-	// "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-prn1/t1/995001_553244524762418_873787853_n.jpg");
-	//
-	// Request.Callback callback = new Request.Callback() {
-	// public void onCompleted(Response response) {
-	// if (response == null)
-	// return;
-	// Log.e("Request.Callback", "Request.Callback response received!");
-	// FacebookRequestError error = response.getError();
-	// if (error != null)
-	// Log.e("Request.Callback", error.getErrorType() + " : " +
-	// error.getErrorMessage());
-	//
-	// JSONObject graphResponse =
-	// response.getGraphObject().getInnerJSONObject();
-	// String postId = null;
-	// try {
-	// postId = graphResponse.getString("id");
-	// } catch (JSONException e) {
-	// Log.i(TAG, "JSON error " + e.getMessage());
-	// }
-	// if (error != null) {
-	// Toast.makeText(getActivity().getApplicationContext(),
-	// error.getErrorMessage(), Toast.LENGTH_SHORT).show();
-	// } else {
-	// Toast.makeText(getActivity().getApplicationContext(), postId,
-	// Toast.LENGTH_LONG).show();
-	// }
-	// }
-	// };
-	//
-	// Request request = new Request(session, "me/feed", postParams,
-	// HttpMethod.POST, callback);
-	//
-	// RequestAsyncTask task = new RequestAsyncTask(request);
-	// task.execute();
-	// }
-	// }
-	//
-	// private boolean isSubsetOf(Collection<String> subset, Collection<String>
-	// superset) {
-	// for (String string : subset) {
-	// if (!superset.contains(string)) {
-	// return false;
-	// }
-	// }
-	// return true;
-	// }
-	//
-	// private void sendRequestDialog() {
-	// Bundle params = new Bundle();
-	// params.putString("message",
-	// "Learn how to make your Android apps social");
-	//
-	// WebDialog requestsDialog = (new
-	// WebDialog.RequestsDialogBuilder(getActivity(),
-	// Session.getActiveSession(), params)).setOnCompleteListener(
-	// new WebDialog.OnCompleteListener() {
-	//
-	// @Override
-	// public void onComplete(Bundle values, FacebookException error) {
-	// if (error != null) {
-	// if (error instanceof FacebookOperationCanceledException) {
-	// Toast.makeText(getActivity().getApplicationContext(),
-	// "Request cancelled", Toast.LENGTH_SHORT).show();
-	// } else {
-	// Toast.makeText(getActivity().getApplicationContext(), "Network Error",
-	// Toast.LENGTH_SHORT).show();
-	// }
-	// } else {
-	// final String requestId = values.getString("request");
-	// if (requestId != null) {
-	// Toast.makeText(getActivity().getApplicationContext(), "Request sent",
-	// Toast.LENGTH_SHORT).show();
-	// } else {
-	// Toast.makeText(getActivity().getApplicationContext(),
-	// "Request cancelled", Toast.LENGTH_SHORT).show();
-	// }
-	// }
-	// }
-	//
-	// }).build();
-	// requestsDialog.show();
-	// }
-
+	
+	private void publishStory() {
+		Session session = Session.getActiveSession();
+		if (session != null) {
+			
+			// Check for publish permissions
+			List<String> permissions = session.getPermissions();
+			if (!isSubsetOf(PERMISSIONS, permissions)) {
+				pendingPublishReauthorization = true;
+				Session.NewPermissionsRequest newPermissionsRequest = new Session.NewPermissionsRequest(this, PERMISSIONS);
+				session.requestNewPublishPermissions(newPermissionsRequest);
+				return;
+			}
+			
+			Bundle postParams = new Bundle();
+			postParams.putString("name", "Facebook SDK for Android");
+			postParams.putString("caption", "Build great social apps and get more installs.");
+			postParams.putString("description", "The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
+			postParams.putString("link", "https://developers.facebook.com/android");
+			
+			String appStorage = Environment.getExternalStorageDirectory().getPath();
+			String path = appStorage + "/PolarClock_background/background.png";
+			// addImage(postParams, path);
+			
+			postParams.putString("picture", "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-prn1/t1/995001_553244524762418_873787853_n.jpg");
+			
+			Request.Callback callback = new Request.Callback() {
+				public void onCompleted(Response response) {
+					if (response == null) return;
+					Log.e("Request.Callback", "Request.Callback response received!");
+					FacebookRequestError error = response.getError();
+					if (error != null) Log.e("Request.Callback", error.getErrorType() + " : " + error.getErrorMessage());
+					
+					JSONObject graphResponse = response.getGraphObject().getInnerJSONObject();
+					String postId = null;
+					try {
+						postId = graphResponse.getString("id");
+					} catch (JSONException e) {
+						Log.i(TAG, "JSON error " + e.getMessage());
+					}
+					if (error != null) {
+						Toast.makeText(getActivity().getApplicationContext(), error.getErrorMessage(), Toast.LENGTH_SHORT).show();
+					} else {
+						Toast.makeText(getActivity().getApplicationContext(), postId, Toast.LENGTH_LONG).show();
+					}
+				}
+			};
+			
+			Request request = new Request(session, "me/feed", postParams, HttpMethod.POST, callback);
+			
+			RequestAsyncTask task = new RequestAsyncTask(request);
+			task.execute();
+		}
+	}
+	
+	private boolean isSubsetOf(Collection<String> subset, Collection<String> superset) {
+		for (String string : subset) {
+			if (!superset.contains(string)) { return false; }
+		}
+		return true;
+	}
+	
+	private void sendRequestDialog() {
+		Bundle params = new Bundle();
+		params.putString("message", "Learn how to make your Android apps social");
+		
+		WebDialog requestsDialog = (new WebDialog.RequestsDialogBuilder(getActivity(), Session.getActiveSession(), params)).setOnCompleteListener(
+				new WebDialog.OnCompleteListener() {
+					
+					@Override
+					public void onComplete(Bundle values, FacebookException error) {
+						if (error != null) {
+							if (error instanceof FacebookOperationCanceledException) {
+								Toast.makeText(getActivity().getApplicationContext(), "Request cancelled", Toast.LENGTH_SHORT).show();
+							} else {
+								Toast.makeText(getActivity().getApplicationContext(), "Network Error", Toast.LENGTH_SHORT).show();
+							}
+						} else {
+							final String requestId = values.getString("request");
+							if (requestId != null) {
+								Toast.makeText(getActivity().getApplicationContext(), "Request sent", Toast.LENGTH_SHORT).show();
+							} else {
+								Toast.makeText(getActivity().getApplicationContext(), "Request cancelled", Toast.LENGTH_SHORT).show();
+							}
+						}
+					}
+					
+				}).build();
+		requestsDialog.show();
+	}
+	
 	public void printHashKey() {
 		try {
 			PackageInfo info = getActivity().getPackageManager().getPackageInfo("ac.jejunu.photify", PackageManager.GET_SIGNATURES);
