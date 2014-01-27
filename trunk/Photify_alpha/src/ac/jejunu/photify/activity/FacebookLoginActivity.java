e248c87369d5bebfe316f2a401c624fcc0c4ba12
pop9310@gmail.com <pop9310@gmail.com@03aedb2d-931b-6bb0-fd80-cf2cc3f0306d>
2014-01-27 06:51:39 +0000

    상세보기 화면 구현중 - 이미지, 댓글 불러오기만 완료. 댓글 작성, 이미지 사이즈 클때 OOM 에러 아직 못고침 지도 클릭시 지도 엑티비티 다시 띄워주면서 마커 올리기..... 풀 투 리프레시 체크 리스트 댓글 작성, 좋아요, 체크 박스 - 체크 리스트를 위한 글 작성시 사진 회전 노티 - 위치 알림 디좌인

--- trunk/Photify_alpha/src/ac/jejunu/photify/activity/FacebookLoginActivity.java
+++ trunk/Photify_alpha/src/ac/jejunu/photify/activity/FacebookLoginActivity.java
@@ -2,27 +2,29 @@ package ac.jejunu.photify.activity;
 
 import ac.jejunu.photify.fragment.FacebookLoginFragment;
 import ac.jejunu.photify.fragment.FacebookLoginFragment_;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class FacebookLoginActivity extends FragmentActivity {
 	private FacebookLoginFragment mainFragment;
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayShowHomeEnabled(false);
+		getActionBar().hide();
+		
 		if (savedInstanceState == null) {
 			// Add the fragment on initial activity setup
 			mainFragment = new FacebookLoginFragment_();
-			getSupportFragmentManager()
-					.beginTransaction()
-					.add(android.R.id.content, mainFragment)
-					.commit();
+			getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
 		} else {
 			// Or set the fragment from restored state info
-			mainFragment = (FacebookLoginFragment) getSupportFragmentManager()
-					.findFragmentById(android.R.id.content);
+			mainFragment = (FacebookLoginFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
 		}
 	}
 }
